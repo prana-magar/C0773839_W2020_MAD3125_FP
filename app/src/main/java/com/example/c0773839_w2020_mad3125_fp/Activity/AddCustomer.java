16b8159243ff9358a461705127f59a29936befac
prana <prakashrana9847067614@gmail.com>
2020-04-20 11:11:32 -0400

    [Prakash] Customer screen empty validations

--- app/src/main/java/com/example/c0773839_w2020_mad3125_fp/Activity/AddCustomer.java
+++ app/src/main/java/com/example/c0773839_w2020_mad3125_fp/Activity/AddCustomer.java
@@ -3,14 +3,89 @@ package com.example.c0773839_w2020_mad3125_fp.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.example.c0773839_w2020_mad3125_fp.Adapter.BillListAdapter;
 import com.example.c0773839_w2020_mad3125_fp.R;
+import com.example.c0773839_w2020_mad3125_fp.Util.PasswordUtil;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class AddCustomer extends AppCompatActivity {
 
+
+    TextInputEditText FirstNameEditText,LastNameEditText,GenderEditText,EmailEditText,PhoneEditText,DobEditText,PasswordEditText;
+    TextInputLayout FirstNameTextInput,GenderTextInput,EmailTextInput,PhoneTextInput,DobTextInput,PasswordTextInput;
+
+    Button CreateButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_customer);
+        FirstNameEditText = findViewById(R.id.FirstNameEditText);
+        LastNameEditText = findViewById(R.id.LastNameEditText);
+        GenderEditText = findViewById(R.id.GenderEditText);
+        EmailEditText = findViewById(R.id.EmailEditText);
+        PhoneEditText = findViewById(R.id.PhoneEditText);
+        DobEditText = findViewById(R.id.DobEditText);
+        PasswordEditText = findViewById(R.id.PasswordEditText);
+        CreateButton = findViewById(R.id.CreateButton);
+
+        FirstNameTextInput = findViewById(R.id.FirstNameTextInput);
+        GenderTextInput = findViewById(R.id.GenderTextInput);
+        EmailTextInput = findViewById(R.id.EmailTextInput);
+        PhoneTextInput = findViewById(R.id.PhoneTextInput);
+        DobTextInput = findViewById(R.id.DobTextInput);
+        PasswordTextInput = findViewById(R.id.PasswordTextInput);
+
+
+
+
+        CreateButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        String firstNameStr = FirstNameEditText.getText().toString();
+                        if(firstNameStr.equals("")){
+                            FirstNameTextInput.setError("First Name cant be Empty");
+                            return;
+                        }
+
+                        String genderStr = GenderEditText.getText().toString();
+                        if(genderStr.equals("")){
+                            GenderTextInput.setError("Gender cant be Empty");
+                            return;
+                        }
+
+                        String emailStr = EmailEditText.getText().toString();
+                        if(emailStr.equals("")){
+                            EmailTextInput.setError("Email cant be Empty");
+                            return;
+                        }
+
+                        String phoneStr = PhoneEditText.getText().toString();
+                        if(phoneStr.equals("")){
+                            PhoneTextInput.setError("PhoneNo cant be Empty");
+                            return;
+                        }
+
+                        String dobStr = DobEditText.getText().toString();
+                        if(dobStr.equals("")){
+                            DobTextInput.setError("Date of Birth cant be Empty");
+                            return;
+                        }
+
+                        String passwordStr = PasswordEditText.getText().toString();
+                        if(passwordStr.equals("")){
+                            PasswordTextInput.setError("Password cant be Empty");
+                            return;
+                        }
+
+
+                    }
+                }
+        );
+
     }
 }
