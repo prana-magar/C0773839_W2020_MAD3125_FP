e3a569072e76ded69624fe7fe0a7a5f030418e6f
prana <prakashrana9847067614@gmail.com>
2020-04-20 11:35:34 -0400

    [Prakash] DOb picker

--- app/src/main/java/com/example/c0773839_w2020_mad3125_fp/Activity/AddCustomer.java
+++ app/src/main/java/com/example/c0773839_w2020_mad3125_fp/Activity/AddCustomer.java
@@ -2,20 +2,25 @@ package com.example.c0773839_w2020_mad3125_fp.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.DatePicker;
 
 import com.example.c0773839_w2020_mad3125_fp.Adapter.BillListAdapter;
 import com.example.c0773839_w2020_mad3125_fp.R;
 import com.example.c0773839_w2020_mad3125_fp.Util.PasswordUtil;
+import com.google.android.material.datepicker.MaterialDatePicker;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 import com.example.c0773839_w2020_mad3125_fp.Util.Validation;
 
-public class AddCustomer extends AppCompatActivity {
+import java.util.Calendar;
+
+public class AddCustomer extends AppCompatActivity implements DatePickerDialog.OnDateSetListener{
 
     String[] genderOptions = new String[] {"Male","Female","Other"};
 
@@ -41,6 +46,23 @@ public class AddCustomer extends AppCompatActivity {
         EmailTextInput = findViewById(R.id.EmailTextInput);
         PhoneTextInput = findViewById(R.id.PhoneTextInput);
         DobTextInput = findViewById(R.id.DobTextInput);
+        final MaterialDatePicker datePicker = new MaterialDatePicker();
+
+        DobEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        AddCustomer.this,
+                        AddCustomer.this,
+                        Calendar.getInstance().get(Calendar.YEAR),
+                        Calendar.getInstance().get(Calendar.MONTH),
+                        Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
+                );
+                datePickerDialog.show();
+            }
+        });
+
+
         PasswordTextInput = findViewById(R.id.PasswordTextInput);
         ArrayAdapter<String> genderAdapter =
                 new ArrayAdapter<String>(
@@ -127,4 +149,9 @@ public class AddCustomer extends AppCompatActivity {
         PasswordTextInput.setError("");
     }
 
+    @Override
+    public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
+        String dateStr = year+"/"+month+"/"+dayOfMonth;
+        DobEditText.setText(dateStr);
+    }
 }
