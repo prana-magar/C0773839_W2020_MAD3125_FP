73db5844c273fe891b2a80644a2f4b10d0070a08
prana <prakashrana9847067614@gmail.com>
2020-04-19 21:26:31 -0400

    [Prakash] Added menu tool bar in customer lister

--- app/src/main/java/com/example/c0773839_w2020_mad3125_fp/Activity/CustomerLister.java
+++ app/src/main/java/com/example/c0773839_w2020_mad3125_fp/Activity/CustomerLister.java
@@ -3,6 +3,12 @@ package com.example.c0773839_w2020_mad3125_fp.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.appcompat.widget.Toolbar;
 
 import com.example.c0773839_w2020_mad3125_fp.R;
 
@@ -12,5 +18,34 @@ public class CustomerLister extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_lister);
+
+        Toolbar myToolbar =  findViewById(R.id.my_toolbar);
+        myToolbar.setTitle("Customers");
+        setSupportActionBar(myToolbar);
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.lister_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId())
+        {
+            case R.id.logout:
+                System.out.println("logout clicked");
+                break;
+
+            case R.id.add_customer:
+                System.out.println("Add new clicked");
+
+        }
+        return true;
     }
 }
